329a520cb37b2e12a6f3a701c57d7959102480dd
RidhamRJ <singhal2003yash@gmail.com>
2023-01-26 21:28:08 +0530

    mewy added

---
+++ try/whttheysay/script.js
@@ -0,0 +1,14 @@
+// Dark & Light toggle
+
+document.querySelector(".day-night input").addEventListener("change", () => {
+    document.querySelector("body").classList.add("toggle");
+    setTimeout(() => {
+      document.querySelector("body").classList.toggle("light");
+  
+      setTimeout(
+        () => document.querySelector("body").classList.remove("toggle"),
+        10
+      );
+    }, 5);
+  });
+  
